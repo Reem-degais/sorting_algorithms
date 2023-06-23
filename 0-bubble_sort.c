840160c5f8c72e1b96ff3f4348b17b7666937eba
Reem-degais <reem.degais.33@gmail.com>
2023-06-23 11:17:08 -0700

    -

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,9 +10,9 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, swap = 0;
 
-	if (size < 2)
+	if (!array || size < 2)
 		return;
-	for (i = 0; i < size; i++)
+	for (i = 0; i < (size - 1); i++)
 		for (j = 0; j < size; j++)
 		{
 			if (array[j] > array[j + 1] && array[j + 1])
