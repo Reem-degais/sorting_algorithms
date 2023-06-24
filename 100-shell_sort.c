d7203764d0b31fef7ce78e2269a2b31ac7e0f521
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-25 02:25:01 +0400

    up

---
+++ 100-shell_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sorts an array of integers in ascending order using the Shell sort algorithm
+ *              with the Knuth sequence.
+ * @array: the array to be sorted.
+ * @size: the size of the array.
+ *
+ * Return: void.
+ */
+void shell_sort(int array[], size_t size)
+{
+    size_t interval = 1;
+
+    while (interval <= size / 3)
+	{
+        interval = interval * 3 + 1;
+    }
+    while (interval > 0) {
+        printf("Interval: %zu\n", interval);
+
+        for (size_t i = interval; i < size; ++i) {
+            int temp = array[i];
+            size_t j = i;
+
+            while (j >= interval && array[j - interval] > temp) {
+                array[j] = array[j - interval];
+                j -= interval;
+            }
+
+            array[j] = temp;
+        }
+        printf("Array: ");
+        for (size_t i = 0; i < size; ++i) {
+            printf("%d ", array[i]);
+        }
+        printf("\n");
+        interval = (interval - 1) / 3;
+    }
+}
