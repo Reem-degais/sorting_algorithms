52aec436bf82d8e86cdd7dd1ccdcd7bf032bf07f
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-28 01:10:45 +0400

    up

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,6 +1,23 @@
 #include "sort.h"
 
 /**
+ * print_subarray - Print a subarray of the given array
+ * @array: The array to be printed
+ * @start: The starting index of the subarray
+ * @end: The ending index of the subarray
+ */
+void print_subarray(int *array, size_t start, size_t end)
+{
+	size_t i;
+	for (i = start; i <= end; i++)
+	{
+		printf("%d", array[i]);
+		if (i != end)
+			printf(", ");
+	}
+}
+
+/**
  * merge - Merge two sorted sub-arrays into a single sorted array
  * @array: The original array
  * @temp: The temporary array used for merging
@@ -11,71 +28,51 @@
  */
 void merge(int *array, int *temp, size_t left_start, size_t left_end, size_t right_start, size_t right_end)
 {
-    size_t i = left_start;
-    size_t j = right_start;
-    size_t k = left_start;
+	size_t i = left_start;
+	size_t j = right_start;
+	size_t k = left_start;
 	size_t x;
 
-    printf("Merging...\n");
-    printf("[left]: ");
-    for (x = left_start; x <= left_end; x++)
-    {
-        printf("%d", array[x]);
-        if (x != left_end)
-            printf(", ");
-    }
-    printf("\n");
-    printf("[right]: ");
-    for (x = right_start; x <= right_end; x++)
-    {
-        printf("%d", array[x]);
-        if (x != right_end)
-            printf(", ");
-    }
-    printf("\n");
-
-    while (i <= left_end && j <= right_end)
-    {
-        if (array[i] <= array[j])
-        {
-            temp[k] = array[i];
-            i++;
-        }
-        else
-        {
-            temp[k] = array[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i <= left_end)
-    {
-        temp[k] = array[i];
-        i++;
-        k++;
-    }
-
-    while (j <= right_end)
-    {
-        temp[k] = array[j];
-        j++;
-        k++;
-    }
-
-    for (x = left_start; x <= right_end; x++)
-    {
-        array[x] = temp[x];
-    }
-
-    printf("[Done]: ");
-    for (x = left_start; x <= right_end; x++)
-    {
-        printf("%d", array[x]);
-        if (x != right_end)
-            printf(", ");
-    }
-    printf("\n");
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_subarray(array, left_start, left_end);
+	printf("\n");
+	printf("[right]: ");
+	print_subarray(array, right_start, right_end);
+	printf("\n");
+	while (i <= left_end && j <= right_end)
+	{
+		if (array[i] <= array[j])
+		{
+			temp[k] = array[i];
+			i++;
+		}
+		else
+		{
+			temp[k] = array[j];
+			j++;
+		}
+		k++;
+	}	
+	while (i <= left_end)
+	{
+		temp[k] = array[i];
+		i++;
+		k++;
+	}	
+	while (j <= right_end)
+	{
+		temp[k] = array[j];
+		j++;
+		k++;
+	}	
+	for (x = left_start; x <= right_end; x++)
+	{
+		array[x] = temp[x];
+	}	
+	printf("[Done]: ");
+	print_subarray(array, left_start, right_end);
+	printf("\n");
 }
 
 /**
@@ -88,16 +85,12 @@ void merge(int *array, int *temp, size_t left_start, size_t left_end, size_t rig
 void merge_sort_helper(int *array, int *temp, size_t start, size_t end)
 {
 	size_t mid;
-
-    if (start >= end)
-    {
-        return;
-    }
-
-    mid = start + (end - start) / 2;
-    merge_sort_helper(array, temp, start, mid);
-    merge_sort_helper(array, temp, mid + 1, end);
-    merge(array, temp, start, mid, mid + 1, end);
+	if (start >= end)
+		return;
+	mid = start + (end - start) / 2;
+	merge_sort_helper(array, temp, start, mid);
+	merge_sort_helper(array, temp, mid + 1, end);
+	merge(array, temp, start, mid, mid + 1, end);
 }
 
 /**
@@ -107,14 +100,12 @@ void merge_sort_helper(int *array, int *temp, size_t start, size_t end)
  */
 void merge_sort(int *array, size_t size)
 {
-    int *temp = malloc(size * sizeof(int));
-    if (temp == NULL)
-    {
-        printf("Memory allocation failed!\n");
-        return;
-    }
-
-    merge_sort_helper(array, temp, 0, size - 1);
-
-    free(temp);
+	int *temp = malloc(size * sizeof(int));
+	if (temp == NULL)
+	{
+		printf("Memory allocation failed!\n");
+		return;
+	}
+	merge_sort_helper(array, temp, 0, size - 1);	
+	free(temp);
 }
