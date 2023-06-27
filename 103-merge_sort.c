4fe23e7631cbd1105a9f94d7e2aa74088cf35d26
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-28 01:15:45 +0400

    up

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -9,6 +9,7 @@
 void print_subarray(int *array, size_t start, size_t end)
 {
 	size_t i;
+
 	for (i = start; i <= end; i++)
 	{
 		printf("%d", array[i]);
@@ -26,7 +27,8 @@ void print_subarray(int *array, size_t start, size_t end)
  * @right_start: The starting index of the right sub-array
  * @right_end: The ending index of the right sub-array
  */
-void merge(int *array, int *temp, size_t left_start, size_t left_end, size_t right_start, size_t right_end)
+void merge(int *array, int *temp, size_t left_start, size_t left_end,
+size_t right_start, size_t right_end)
 {
 	size_t i = left_start;
 	size_t j = right_start;
@@ -53,23 +55,23 @@ void merge(int *array, int *temp, size_t left_start, size_t left_end, size_t rig
 			j++;
 		}
 		k++;
-	}	
+	}
 	while (i <= left_end)
 	{
 		temp[k] = array[i];
 		i++;
 		k++;
-	}	
+	}
 	while (j <= right_end)
 	{
 		temp[k] = array[j];
 		j++;
 		k++;
-	}	
+	}
 	for (x = left_start; x <= right_end; x++)
 	{
 		array[x] = temp[x];
-	}	
+	}
 	printf("[Done]: ");
 	print_subarray(array, left_start, right_end);
 	printf("\n");
@@ -85,6 +87,7 @@ void merge(int *array, int *temp, size_t left_start, size_t left_end, size_t rig
 void merge_sort_helper(int *array, int *temp, size_t start, size_t end)
 {
 	size_t mid;
+
 	if (start >= end)
 		return;
 	mid = start + (end - start) / 2;
@@ -94,18 +97,19 @@ void merge_sort_helper(int *array, int *temp, size_t start, size_t end)
 }
 
 /**
- * merge_sort - Sorts an array of integers in ascending order using the Merge sort algorithm
+ * merge_sort - Sorts an array of integers in ascending order
  * @array: The array to be sorted
  * @size: The size of the array
  */
 void merge_sort(int *array, size_t size)
 {
 	int *temp = malloc(size * sizeof(int));
+
 	if (temp == NULL)
 	{
 		printf("Memory allocation failed!\n");
 		return;
 	}
-	merge_sort_helper(array, temp, 0, size - 1);	
+	merge_sort_helper(array, temp, 0, size - 1);
 	free(temp);
 }
